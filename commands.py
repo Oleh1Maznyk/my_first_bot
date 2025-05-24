7b224070a3f691c7d3d5785963b0831e064ed29f
Oleh1Maznyk <maznykoleh283@gmail.com>
2025-05-24 23:01:44 +0200

    Added a new button to the menu (not yet implemented)

--- commands.py
+++ commands.py
@@ -2,10 +2,12 @@ from aiogram.filters import Command
 from aiogram.types.bot_command import BotCommand
 
 
-FILM_COMMAND = Command("films")
 START_COMMAND = Command("start")
+FILM_COMMAND = Command("films")
+ADD_FILM_COMMAND = Command("add_film")
 
 COMMANDS = [
+    BotCommand(command="start", description="Запуск програми"),
     BotCommand(command="films", description="Показати всі вільми"),
-    BotCommand(command="start", description="Запуск програми")
+    BotCommand(command="add_film", description="Додати новий фільм")
 ]
